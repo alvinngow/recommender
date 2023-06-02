da411db58b640a192f9c500fc393a5b515380d51
icedbluw <alvinngow21@gmail.com>
2023-06-03 00:14:28 +0800

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { analytics } from "./analytics";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "RecPlusAI",
-	description: "Social Media with AI",
+	title: "RecplusAI",
+	description: "Simplifying commendations using A.I",
 };
 
 export default function RootLayout({
@@ -13,6 +17,9 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
+	useEffect(() => {
+		analytics(window, document, "script", "dataLayer", "G-GMPFRNH8JJ");
+	});
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>

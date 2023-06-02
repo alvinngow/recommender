3ae52852b2b2d11da2817b28a6ad38b86d412bfe
icedbluw <alvinngow21@gmail.com>
2023-06-03 01:42:31 +0800

    no nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function NavBar() {
 	return (
 		<div className="absolute top-0 px-24 w-full">
-			<Image src="/logo.png" className="pt-2" width={234} height={60}></Image>
+			{/* <Image src="/logo.png" className="pt-2" width={234} height={60}></Image> */}
 		</div>
 	);
 }

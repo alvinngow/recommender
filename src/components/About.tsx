3e563847e1206921bcd44f14abe6ced1fa6f1b2a
icedbluw <alvinngow21@gmail.com>
2023-06-13 10:53:26 +0800

    fix about on mobile

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,19 +1,19 @@
 function About() {
 	return (
-		<div className="flex min-h-screen justify-center  p-24">
-			<div className="flex items-center flex-col w-1/2 text-left">
+		<div className="flex min-h-screen justify-center pt-24 lg:p-24">
+			<div className="flex items-center flex-col lg:w-1/2 px-2 text-left">
 				<p className="text-4xl">About Us</p>
-				<p className="mt-4 text-2xl text-center">
+				<p className="mt-4 text-lg lg:text-2xl text-center">
 					Founded by two Information Systems professionals with a vision to
 					bridge technology laggard&apos;s problems using generative A.I
 				</p>
-				<p className="mt-4 text-2xl text-center">
+				<p className="mt-4 text-lg lg:text-2xl text-center">
 					With Recplus.AI, we aim to provide an easy-to-use structured interface
 					where undergraduates and mid-career job seekers can get professional
 					recommendations to stand out against their competition. Commendations
 					are best given before receiving.
 				</p>
-				<p className="mt-4 text-2xl text-center">
+				<p className="mt-4 text-lg lg:text-2xl text-center">
 					We often overlook beyond the &quot;Five Cs of Singapore&quot; —
 					namely, cash, car, credit card, condominium and country club
 					membership. There is no doubt in our hearts that when it comes to
